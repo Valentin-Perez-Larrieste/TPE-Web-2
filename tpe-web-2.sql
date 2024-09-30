d5e46d8c02b3ba33762ec07a1d8a1479f8403935
juani117 <lol_igna_022@outlook.com>
2024-09-30 12:33:32 -0300

    se agregaron mas libros a catalogo0

--- tpe-web-2.sql
+++ tpe-web-2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 13-09-2024 a las 21:30:51
+-- Tiempo de generación: 30-09-2024 a las 17:30:58
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -32,7 +32,7 @@ CREATE TABLE `libro` (
   `nombre` varchar(60) NOT NULL,
   `autor` varchar(60) NOT NULL,
   `editorial` varchar(60) DEFAULT NULL,
-  `genero` varchar(20) NOT NULL,
+  `genero` varchar(60) NOT NULL,
   `stock` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -41,9 +41,30 @@ CREATE TABLE `libro` (
 --
 
 INSERT INTO `libro` (`id`, `nombre`, `autor`, `editorial`, `genero`, `stock`) VALUES
-(1, 'Don Quijote de la Mancha', 'Miguel de Cervantes', 'Herrero Hermanos Saturnino Calleja', 'parodia', 15),
-(2, 'La Regenta.', 'Leopoldo Arias Clarín', 'Alba Editorial', 'ficcion', 10),
-(3, 'La cartuja de Parma', 'Stendhal', NULL, 'ficcion', 5);
+(1, 'Don Quijote de la Mancha', 'Miguel de Cervantes', 'Herrero Hermanos Saturnino Calleja', 'Parodia,Humor,Comedia', 15),
+(2, 'La Regenta.', 'Leopoldo Arias Clarín', 'Alba Editorial', 'Ficcion', 10),
+(3, 'La cartuja de Parma', 'Stendhal', NULL, 'Ficcion', 5),
+(4, 'Drácula', 'Bram Stoker', 'Editorial Anto', 'Terror, Novela', 5),
+(5, 'Cementerio de animales', 'Stephen King', 'Alfaguara', 'Terror', 3),
+(6, 'Soy leyenda', 'Richard Matheson', 'Minotauro', 'Ciencia ficcion,Terror', 2),
+(7, 'El misterio de Salem\'s Lot', 'Stephen King', 'DEBOLS!LLO', 'Novela, Terror', 1),
+(8, 'La conjura de los necios', 'John Kennedy Toole', 'Editorial Anagrama', ' Novela, Humor, Comedia', 0),
+(9, '50 sombras de Luisi', 'Ángel Sanchidrián', 'Minotauro', 'Humor,Comedia', 3),
+(10, 'El proyecto esposa', 'Graeme Simsion', 'SALAMANDRA', 'Novela rosa, Romance', 10),
+(11, 'Días de perros', 'Gilles Legardinier', 'Editorial Planeta', ' Ficcion, Humor', 3),
+(12, 'Guía del autoestopista galáctico', 'Robbie Stamp, Douglas Adams', 'Editorial Anagrama', ' Ciencia ficcion, Novela, Humor', 2),
+(13, 'Esnob', 'Elísabet Benavent', 'SUMA', 'Novela rosa, Ficcion', 15),
+(14, 'Forastera', 'Diana Gabaldon', 'SALAMANDRA BOLSILLO', 'Novela,Romance', 2),
+(15, 'La Casa Neville 2. No quieras nada vil', 'Florencia Bonelli', NULL, 'Novela rosa, Ficcion', 2),
+(16, 'Dune', 'Frank Herbert', 'LA FACTORÍA DE IDEAS', 'Novela, Ciencia ficcion', 3),
+(17, 'Fahrenheit 451', 'Ray Bradbury', 'Minotauro', 'Novela, Ciencia ficcion', 2),
+(18, 'El marciano', 'Andy Weir', 'B de Books (Ediciones B)', ' Ciencia ficcion, Novela', 1),
+(19, 'Un mundo feliz', 'Aldous Huxley', NULL, 'Novela, Ciencia ficcion', 2),
+(20, 'El eternauta', 'H.G.Oesterheld, Solano López', 'Planeta Cómic Argentina', 'Historieta, Novela grafica', 5),
+(21, 'Asesinato en el Orient Express', 'Agatha Christie', 'Grupo Planeta - Argentina', 'Novela, Ficcion detectivesca, Misterio, Policial', 2),
+(23, 'Holly (Edición en español)', 'Stephen King', 'PLAZA & JANÉS', 'Terror, Misterio, Suspenso', 2),
+(24, 'El Psicoanalista', 'John Katzenbach', 'Penguin Random House Grupo Editorial España', 'Suspenso, Novela psicologica', 0),
+(25, 'El visitante', 'Stephen King', 'DEBOLS!LLO', 'Novela, Terror', 3);
 
 -- --------------------------------------------------------
 
@@ -89,8 +110,8 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`id`, `nombre`, `apellido`, `dni`, `telefono`, `email`, `estado`) VALUES
-(1, 'juan', 'muñiz', 44764321, 596532, 'uni.juan@com', 'no deudor'),
-(2, 'valen', 'que te importa', 32546315, 423567, 'valenqueteimporta@com', 'deudor'),
+(1, 'Juan', 'Muñiz', 44764321, 596532, 'uni.juan@com', 'no deudor'),
+(2, 'Valen', 'Perez', 32546315, 423567, 'valenqueteimporta@com', 'deudor'),
 (3, 'pepe', 'pepon', 43865743, 543212, 'pepe.pepom@.com', 'no deudor');
 
 --
@@ -128,7 +149,7 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `libro`
 --
 ALTER TABLE `libro`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT de la tabla `solicitud`
